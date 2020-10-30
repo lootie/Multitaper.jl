94846d431e0ff0891a9667aadd6e7bf20d026db4
Charlotte Haley <haleycharlotte@gmail.com>
2020-10-30 14:41:20 -0500

    finishing hosting for documenter.jl, hopefully

--- docs/make.jl
+++ docs/make.jl
@@ -10,8 +10,8 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
 
+deploydocs(
+    repo = "github.com/lootie/lootie.github.io.git",
+)
 
