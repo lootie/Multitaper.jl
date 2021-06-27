6665ebb0905e81a0b3ffb4c4783797d8e8e82c7a
Charlotte Haley <haleycharlotte@gmail.com>
2021-06-26 20:45:47 -0500

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,24 +1,11 @@
 using Documenter, Multitaper
 
-makedocs(
-    modules = [Multitaper],
-    sitename = "Multitaper",
-    repo = "https://github.com/lootie/multitaper.jl/src/{commit}{path}#lines-{line}",
+makedocs()
+
+deploydocs(
+    repo = "github.com/lootie/lootie.bitbucket.io.git",
 )
 
-withenv(
-    "TRAVIS_REPO_SLUG" => "github.com/lootie/lootie.bitbucket.io",
-    "TRAVIS_PULL_REQUEST" => get(ENV, "BITBUCKET_BRANCH", nothing),
-    "TRAVIS_BRANCH" => get(ENV, "BITBUCKET_BRANCH", nothing),
-    "TRAVIS_TAG" => get(ENV, "BITBUCKET_TAG", nothing),
-    "TRAVIS_PULL_REQUEST" => ("BITBUCKET_PR_ID" in keys(ENV)) ? "true" : "false",
-) do
-    deploydocs(
-        repo = "github.com/lootie/lootie.bitbucket.io.git",
-        branch = "gh-pages",
-        dirname = "Multitaper.jl",
-        devbranch = "master"
-    )
 end
 
 
