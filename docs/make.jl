83c484266261358f8adbc451c5e051ae819fe367
Charlotte Haley <haleycharlotte@gmail.com>
2021-06-26 21:06:36 -0500

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,11 +1,17 @@
 using Documenter, Multitaper
 
-makedocs()
-
-deploydocs(
-    repo = "github.com/lootie/Multitaper.jl.git",
+makedocs(
+    modules = [Multitaper],
+    sitename = "Multitaper",
+    repo = "https://github.com/lootie/multitaper.jl/src/{commit}{path}#lines-{line}",
 )
 
-end
+deploydocs(
+    repo = "github.com/lootie/lootie.github.io",
+    branch = "gh-pages",
+    dirname = "Multitaper.jl",
+    devbranch = "master"
+)
+
 
 
