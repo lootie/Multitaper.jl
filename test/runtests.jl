0d0ced3acc4d1f0122ab586b74cb0d63298e8ad2
Charlotte Haley <haleycharlotte@gmail.com>
2021-08-23 14:42:16 -0500

    notebook

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,8 @@ using DelimitedFiles, LinearAlgebra, Statistics
 
 printstyled("Running tests:\n", color=:blue)
 
-tests = ["utils", "univariate", "crossings", "mdmwps", "multivariate", "twodim", "bronez"]
+tests = ["utils", "univariate", "crossings", #"mdmwps", 
+         "multivariate", "twodim"] #, "bronez"]
 
 for t in tests
     @testset "$t multitaper" begin
